5f6c979166c050e02eca042c3a7819c913af8887
Crowdbotics <team@crowdbotics.com>
2023-04-24 12:10:27 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/testing_taxi_app_40160/MainActivity.java
@@ -0,0 +1,15 @@
+package com.testing_taxi_app_40160;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "testing_taxi_app_40160";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "testing-taxi-app-40160",
+  url: "https://testing-taxi-app-40160.botics.co",
+  api: "https://testing-taxi-app-40160.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
